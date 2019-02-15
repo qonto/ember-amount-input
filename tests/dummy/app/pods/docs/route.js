5fc4317226015de1dd717d649433f853855b81d4
hakilebara <frederic.soumare@gmail.com>
2019-02-15 11:51:54 +0100

    setup ec-addon-docs homepage

--- tests/dummy/app/pods/docs/route.js
+++ tests/dummy/app/pods/docs/route.js
@@ -1,3 +1,7 @@
 import DocsRoute from 'ember-cli-addon-docs/routes/docs'
 
-export default class DocsIndexRoute extends DocsRoute {}
+export default class DocsIndexRoute extends DocsRoute {
+  beforeModel() {
+    this.replaceWith('docs.installation')
+  }
+}
