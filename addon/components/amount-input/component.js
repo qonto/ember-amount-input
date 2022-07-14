30f4d76bbcb66c3a8b226da66261578d48a997f3
mrloop <ewan.mcdougall.ext@qonto.com>
2022-07-14 12:44:49 +0100

    Remove @classNames decorator

--- addon/components/amount-input/component.js
+++ addon/components/amount-input/component.js
@@ -1,7 +1,4 @@
-import {
-  classNames,
-  layout as templateLayout
-} from '@ember-decorators/component'
+import { layout as templateLayout } from '@ember-decorators/component'
 import Component from '@ember/component'
 import layout from './template'
 
@@ -20,7 +17,6 @@ const KEY_CODE_COMMA = 188
   @public
 */
 @templateLayout(layout)
-@classNames('amount-input')
 export default class AmountInput extends Component {
   /**
     The currency displayed in the input
