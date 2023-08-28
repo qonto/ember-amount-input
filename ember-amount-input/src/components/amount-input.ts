ee05c6154502a0af3eb34916d604bd7e7a9b1195
Vincent Scavinner <vincent.scavinner@qonto.com>
2023-08-28 21:25:24 +0200

    feat(ts): map component names used in templates to the actual backing value they resolved to

--- ember-amount-input/src/components/amount-input.ts
+++ ember-amount-input/src/components/amount-input.ts
@@ -180,3 +180,10 @@ export default class AmountInput extends Component<AmountInputSignature> {
     return defaultValue;
   }
 }
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    AmountInput: typeof AmountInput;
+    'amount-input': typeof AmountInput;
+  }
+}
