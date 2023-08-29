8bfe3c08b9db27ca20a4e5c02c63d47fd8707e80
Vincent Scavinner <vincent.scavinner@qonto.com>
2023-08-29 22:25:59 +0200

    chore: drop rollup-plugin-ts and use updated addon blueprint

--- ember-amount-input/rollup.config.mjs
+++ ember-amount-input/rollup.config.mjs
@@ -1,4 +1,4 @@
-import typescript from 'rollup-plugin-ts';
+import { babel } from '@rollup/plugin-babel';
 import copy from 'rollup-plugin-copy';
 import { Addon } from '@embroider/addon-dev/rollup';
 
@@ -6,6 +6,9 @@ const addon = new Addon({
   srcDir: 'src',
   destDir: 'dist',
 });
+
+// Add extensions here, such as ts, gjs, etc that you may import
+const extensions = ['.js', '.ts'];
 
 export default {
   // This provides defaults that work well alongside `publicEntrypoints` below.
@@ -15,7 +18,7 @@ export default {
   plugins: [
     // These are the modules that users should be able to import from your
     // addon. Anything not listed here may get optimized away.
-    addon.publicEntrypoints(['components/**/*.ts']),
+    addon.publicEntrypoints(['components/**/*.js', 'template-registry.js']),
 
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
@@ -27,11 +30,13 @@ export default {
     // package names.
     addon.dependencies(),
 
-    // Compile TypeScript to latest JavaScript, including Babel transpilation
-    typescript({
-      transpiler: 'babel',
-      browserslist: false,
-      transpileOnly: false,
+    // This babel config should *not* apply presets or compile away ES modules.
+    // It exists only to provide development niceties for you, like automatic
+    // template colocation.
+    // See `babel.config.json` for the actual Babel configuration!
+    babel({
+      extensions,
+      babelHelpers: 'bundled',
     }),
 
     // Ensure that standalone .hbs files are properly integrated as Javascript.
