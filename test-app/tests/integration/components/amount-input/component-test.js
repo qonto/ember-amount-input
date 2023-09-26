2f267c51638097b259c970d795f50dd1d1f58a58
Vincent Scavinner <vincent.scavinner@qonto.com>
2023-09-26 18:31:55 +0200

    refactor: rewrite test descriptions in component integration tests

--- test-app/tests/integration/components/amount-input/component-test.js
+++ test-app/tests/integration/components/amount-input/component-test.js
@@ -6,16 +6,16 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | amount-input', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('Input should be of type number', async function (assert) {
+  test('is of type number', async function (assert) {
     await render(hbs`
-      <AmountInput/>
+      <AmountInput />
     `);
 
     assert.dom('.amount-input').exists;
     assert.dom('input').hasAttribute('type', 'number');
   });
 
-  test('Arguments work properly', async function (assert) {
+  test('allows passing arguments', async function (assert) {
     this.set('value', 1);
 
     await render(hbs`
@@ -45,7 +45,7 @@ module('Integration | Component | amount-input', function (hooks) {
     assert.dom('input').hasAttribute('max', '10');
   });
 
-  test('defaults are not used if named property declared even if undefined', async function (assert) {
+  test('uses named property declared (even if undefined) instead of defaults', async function (assert) {
     await render(hbs`
       <AmountInput
         @currency={{this.isUndefined}}
@@ -54,55 +54,57 @@ module('Integration | Component | amount-input', function (hooks) {
         @step={{this.isUndefined}}
         @inputId={{this.isUndefined}}
         @value={{this.value}}
-        @update={{fn (mut this.value)}} />
+        @update={{fn (mut this.value)}}
+      />
     `);
+
     assert.dom('.amount-input__currency').hasText('');
     assert.dom('input').hasNoAttribute('placeholder');
     assert.dom('input').hasNoAttribute('id');
 
-    // test numberOfDecimal
     await fillIn('input', '10.726');
     await blur('input');
 
     assert.dom('input').hasValue('11');
   });
 
-  test('defaults are used if named property not declared', async function (assert) {
+  test('uses defaults if named property are not declared', async function (assert) {
     await render(hbs`
       <AmountInput
         @value={{this.value}}
-        @update={{fn (mut this.value)}} />
+        @update={{fn (mut this.value)}}
+      />
     `);
 
     assert.dom('.amount-input__currency').hasText('EUR');
     assert.dom('input').hasAttribute('placeholder', '0.00');
     assert.dom('input').hasAttribute('id', 'amount-input');
 
-    // test numberOfDecimal
     await fillIn('input', '10.726');
     await blur('input');
 
     assert.dom('input').hasValue('10.73');
   });
 
-  test('Should not type in when readonly attribute is true', async function (assert) {
+  test('does not type in when readonly attribute is set to true', async function (assert) {
     await render(hbs`
       <AmountInput
         @value={{this.value}}
         @update={{fn (mut this.value)}} 
-        @readonly={{true}} />
+        @readonly={{true}}
+      />
     `);
 
-    // test type in
     assert.rejects(fillIn('input', '10.726'));
   });
 
-  test('should call update with the formatted value on blur if the value is 0', async function (assert) {
+  test('calls update with the formatted value on blur if the value is 0', async function (assert) {
     await render(hbs`
-    <AmountInput
-    @numberOfDecimal={{1}}
-    @value={{this.value}}
-    @update={{fn (mut this.value)}} />
+      <AmountInput
+        @numberOfDecimal={{1}}
+        @value={{this.value}}
+        @update={{fn (mut this.value)}}
+      />
     `);
 
     await fillIn('input', '0');
@@ -110,7 +112,7 @@ module('Integration | Component | amount-input', function (hooks) {
     assert.dom('input').hasValue('0.0');
   });
 
-  test('e, . and , key should be masked when numberOfDecimal={{0}}', async function (assert) {
+  test('masks `e`, `.` and `,` keys when `numberOfDecimal` argument is set to 0', async function (assert) {
     this.set('value', 1);
 
     await render(hbs`
